766bee295d7d22bdf2bf79c147547664618d6f45
Sebastian Schindler <sebastian.schindler@fau.de>
2023-12-08 14:31:58 +0100

    blank/tab conversion

--- ipynb/tools.py
+++ ipynb/tools.py
@@ -43,60 +43,60 @@ def cache_file(url):
 import astropy.coordinates as coord
 import astropy.units as u
 def plt_skyplot(ra, dec, galactic=False, galaxy=False, figsize=[16,8], **kwargs):
-    """Create skyplot in equatorial or galactic coordinates from arrays of RA and Dec values in degrees."""
-    
-    def trafo_equ(ra):
-        ra = ra + 180*u.deg
-        ra = ra.wrap_at(180*u.deg)
-        ra = -ra
-        return ra
-    def trafo_gal(l):
-        l = l.wrap_at(180*u.deg)
-        l = -l
-        return l
-    
-    plt.figure(figsize=figsize)
-    plt.subplot(projection='mollweide')
-    plt.grid()
-    
-    def set_xticks(left, right):
-        
-        xticks = np.linspace(-180, 180, 13) # 30 deg steps incl. 0 and bounds
-        xticks = np.deg2rad(xticks)
-        xticks = xticks[1:-1] # exclude bounds
-        
-        xticklabels = []
-        for number in np.linspace(left, right, 13):
-            xticklabels.append(u"%d \N{DEGREE SIGN}" % number)
-        xticklabels = xticklabels[1:-1]
-        
-        plt.xticks(xticks, xticklabels)
-    
-    coords = coord.SkyCoord(ra*u.deg, dec*u.deg)
-    if galactic:
-        coords = coords.transform_to('galactic')
-        coord_x = trafo_gal(coords.l)
-        coord_y = coords.b
-        set_xticks(180, -180)
-        plt.text(0.9, 0.1, "Galactic", transform=plt.figure(1).axes[0].transAxes)
+	"""Create skyplot in equatorial or galactic coordinates from arrays of RA and Dec values in degrees."""
+	
+	def trafo_equ(ra):
+		ra = ra + 180*u.deg # type: ignore
+		ra = ra.wrap_at(180.*u.deg)
+		ra = -ra
+		return ra
+	def trafo_gal(l):
+		l = l.wrap_at(180*u.deg)
+		l = -l
+		return l
+	
+	plt.figure(figsize=figsize)
+	plt.subplot(projection='mollweide')
+	plt.grid()
+	
+	def set_xticks(left, right):
+		
+		xticks = np.linspace(-180, 180, 13) # 30 deg steps incl. 0 and bounds
+		xticks = np.deg2rad(xticks)
+		xticks = xticks[1:-1] # exclude bounds
+		
+		xticklabels = []
+		for number in np.linspace(left, right, 13):
+			xticklabels.append(u"%d \N{DEGREE SIGN}" % number)
+		xticklabels = xticklabels[1:-1]
+		
+		plt.xticks(xticks, xticklabels)
+	
+	coords = coord.SkyCoord(ra*u.deg, dec*u.deg)
+	if galactic:
+		coords = coords.transform_to('galactic')
+		coord_x = trafo_gal(coords.l)
+		coord_y = coords.b
+		set_xticks(180, -180)
+		plt.text(0.9, 0.1, "Galactic", transform=plt.figure(1).axes[0].transAxes)
  
-    else:
-        coord_x = trafo_equ(coords.ra)
-        coord_y = coords.dec
-        set_xticks(360, 0)
-        plt.text(0.9, 0.1, "Equatorial", transform=plt.figure(1).axes[0].transAxes)
+	else:
+		coord_x = trafo_equ(coords.ra)
+		coord_y = coords.dec
+		set_xticks(360, 0)
+		plt.text(0.9, 0.1, "Equatorial", transform=plt.figure(1).axes[0].transAxes)
 
-    plt.scatter(coord_x.rad, coord_y.rad, **kwargs)
-    
-    if galaxy:
-        
-        plane = coord.SkyCoord(frame='galactic', l=np.linspace(0, 360, 100)*u.deg, b=0*u.deg)
-        plane = plane.transform_to('icrs')
-        plt.plot(trafo_equ(plane.ra).rad, plane.dec.rad, 'black', linewidth=.5)
+	plt.scatter(coord_x.rad, coord_y.rad, **kwargs)
+	
+	if galaxy:
+		
+		plane = coord.SkyCoord(frame='galactic', l=np.linspace(0, 360, 100)*u.deg, b=0*u.deg)
+		plane = plane.transform_to('icrs')
+		plt.plot(trafo_equ(plane.ra).rad, plane.dec.rad, 'black', linewidth=.5)
 
-        center = coord.SkyCoord(frame='galactic', l=0*u.deg, b=0*u.deg)
-        center = center.transform_to('icrs')
-        plt.plot(trafo_equ(center.ra).rad, center.dec.rad, '*k', markersize=5)
+		center = coord.SkyCoord(frame='galactic', l=0*u.deg, b=0*u.deg)
+		center = center.transform_to('icrs')
+		plt.plot(trafo_equ(center.ra).rad, center.dec.rad, '*k', markersize=5)
 
 
 import pickle as pkl
